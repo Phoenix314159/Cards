b66a094b97e2e89d4853876510fe0b9c48ae645e
James Langford <derivative7117@live.com>
2017-10-21 19:45:32 -0600

    added deal function

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
-
-
-
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	cards.print()
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
