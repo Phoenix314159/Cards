67c7787597ebdbac0fcc3fc105ca42e1a714fbf4
James Langford <derivative7117@live.com>
2017-10-24 23:27:54 -0600

    added testing

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ func main() {
 	deckTwo.print()
 	fmt.Println("===============")
 	//deckTwo = append(deckTwo, "Ace of Diamonds")  // like .push in js but doesn't mutate original slice
-	deckThree := newDeck()  //deckThree is now of type 'deck' since newDeck() returns a type of 'deck'
+	deckThree := newDeck() //deckThree is now of type 'deck' since newDeck() returns a type of 'deck'
 	//deckThree.print()  // therefore it has access to the print method
 	hand, remainingDeck := deal(deckThree, 5) // first value returned will be assigned to hand
-	hand.print() //hand is of type 'deck'
+	hand.print()                              //hand is of type deck
 	fmt.Println("===============")
 	s := remainingDeck.toString() // remainingDeck is of type deck
-    fmt.Println(s)
+	fmt.Println(s)
 	fmt.Println("===============")
 	greeting := "Hi There!!"
 	bs := []byte(greeting) //type conversion from string to a byte slice
